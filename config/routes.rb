fe8e73b3f44e2fd5911a14c3bd6affa547b0823e
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-08 07:44:24 +0000

    [add]routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  resources :homes, :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
