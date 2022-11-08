d35d3a76c0ee78ecd28bb098fc2658e25036d7ff
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-08 12:52:33 +0000

    [add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :homes, :books
   root to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
